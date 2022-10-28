58dad132521fec244b8f8f2515a28bce41a17977
Raihangarin <100213839+Raihangarin@users.noreply.github.com>
2022-10-28 14:45:57 +0700

    Add files via upload

---
+++ PPL.java
@@ -0,0 +1,12 @@
+public class Mobil{
+
+    Int KodeBuku=2;
+    String Judul="Sejarah Jawa";
+    String Pengarang="Rizal";
+    Int TahunTerbit=2020;
+    Int Stok=20;
+
+    void tambah(){
+        System.out.println("Buku"+Judul+"menambha stok"+(stok+1)+)
+    }
+}
